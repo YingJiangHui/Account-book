e749bec3806d36e141ba3e6a521e2a942e1c286b
dark <473380917@qq.com>
2020-08-20 17:05:51 +0800

    将按钮抽出来创建为Button组件

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="button-wrapper">
+        <button class="button" @click="$emit('click',$event)">
+            <slot/>
+        </button>
+    </div>
+</template>
+
+<script lang='ts'>
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Button extends Vue {
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+    .button {
+        border-radius: 4px;
+        height: 40px;
+        padding: 0 16px;
+        border: none;
+        background: #767676;
+        color: white;
+
+        &-wrapper {
+            padding: 16px;
+            text-align: center;
+            margin: 44-16px;
+        }
+    }
+</style>
