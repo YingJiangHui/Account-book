6a9b0dc6e13c1d495bcbd5f9208ce1b826623649
dark <473380917@qq.com>
2020-09-12 10:07:31 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 构建
-# npm run build
+yarn build
 
 # cd 到构建输出的目录下
 cd dist
@@ -12,9 +12,9 @@ cd dist
 # 部署到自定义域域名
 # echo 'www.example.com' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
